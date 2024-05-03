d3d6e6a8996e396f3e634995ac8e87ca847d9624
Quinn Unger <quinn.unger@pm.me>
2024-05-03 17:38:58 +0100

    use f string formatting

--- linter.py
+++ linter.py
@@ -186,7 +186,7 @@ class Verilator(Linter):
                     for path in prjsrc:
                         if sublime.platform() == 'windows':
                             if wslopt:
-                                path = re.sub(r'(^[a-zA-Z])(:)(.*$)',lambda m : ''.join(['/mnt/',m.group(1).lower(),m.group(3)]),path)
+                                path = re.sub(r'(^[a-zA-Z])(:)(.*$)',lambda m : f'/mnt/{m.group(1).lower()}{m.group(3)}',path)
                             path = re.sub(re.compile(r'\\'), '/', path)
                         path = '-I' + path
                         cmd.append(path)
@@ -197,7 +197,7 @@ class Verilator(Linter):
                 if sublime.platform() == 'windows':
                     if wslopt:
                         orig_file = file.name
-                        file.name = re.sub(r'(^[a-zA-Z])(:)(.*$)',lambda m : ''.join(['/mnt/',m.group(1).lower(),m.group(3)]),file.name)
+                        file.name = re.sub(r'(^[a-zA-Z])(:)(.*$)',lambda m : f'/mnt/{m.group(1).lower()}{m.group(3)}',file.name)
                     file.name = re.sub(re.compile(r'\\'), '/', file.name)
                 ctx['temp_file'] = file.name
                 cmd.append(file.name)
@@ -220,8 +220,8 @@ class Verilator(Linter):
                         if wslopt:
                             orig_file = file.name
                             orig_wrap = wrap.name
-                            file.name = re.sub(r'(^[a-zA-Z])(:)(.*$)',lambda m : ''.join(['/mnt/',m.group(1).lower(),m.group(3)]),file.name)
-                            wrap.name = re.sub(r'(^[a-zA-Z])(:)(.*$)',lambda m : ''.join(['/mnt/',m.group(1).lower(),m.group(3)]),wrap.name)
+                            file.name = re.sub(r'(^[a-zA-Z])(:)(.*$)',lambda m : f'/mnt/{m.group(1).lower()}{m.group(3)}',file.name)
+                            wrap.name = re.sub(r'(^[a-zA-Z])(:)(.*$)',lambda m : f'/mnt/{m.group(1).lower()}{m.group(3)}',wrap.name)
                         file.name = re.sub(re.compile(r'\\'), '/', file.name)
                         wrap.name = re.sub(re.compile(r'\\'), '/', wrap.name)
                     ctx['temp_file'] = file.name
